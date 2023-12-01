71d81a2654d79411bbc7c9d93f69555efd9fd027
Yina51 <ny0510@hansung.ac.kr>
2023-12-01 15:20:29 +0900

    index.js에 add함수 수정, substrac 함수 추가)

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /*
     index.html을 위한 js 파일
 */
-function add(a, b) {
-    return a + b;
+function add(a, b, c) {
+    if (c !== undefined) {
+        return a + b;
+    }
+}
+
+function substract(a,b) {
+    if (a > b) {
+        return a - b;
+    } else b - a;
 }
